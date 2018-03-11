531d162afdc7bcebf9664a0fc48122e0c193c587
nguyentrunghieu <nguyenhieu.12a4@gmail.com>
2018-03-11 11:38:13 +0700

    setup theme canapapa

--- wp-content/themes/canapapa/functions.php
+++ wp-content/themes/canapapa/functions.php
@@ -57,7 +57,6 @@ add_action('wp_enqueue_scripts', 'canapapa_script_enqueue');
 add_theme_support('post-thumbnails');
 add_theme_support('custom-background');
 add_theme_support('post-formats', array('aside', 'image', 'video'));
-add_theme_support('html5', 'search');
 
 /**
  * Add custom thumbnail sizes
