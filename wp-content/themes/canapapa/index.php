48655328f8950723cd24b39d5d7508ea15fbffc7
nguyentrunghieu <nguyenhieu.12a4@gmail.com>
2018-03-14 01:14:45 +0700

    setup theme canapapa

--- wp-content/themes/canapapa/index.php
+++ wp-content/themes/canapapa/index.php
@@ -1,6 +1,16 @@
-<?php
-    get_header()
-?>
+<?php get_header(); ?>
+<div class="content-wrap" data-effect="lnl-push">
+    <?php get_template_part('header', 'canapapa'); ?>
+    <nav class="navbar navbar-default navbar-static-top line-navbar-two">
+        <div class="container">
+            <div class="collapse navbar-collapse" id="line-navbar-collapse-2">
+                <!--        menu    -->
+                <?php get_template_part('menu'); ?>
+                <!--        search    -->
+                <?php get_template_part( 'search'); ?>
+            </div>
+        </div>
+    </nav>
     <?php
     if ( have_posts() ) :
         get_template_part( 'content');
@@ -9,6 +19,7 @@
     endif;
 
     ?>
-<?php
-    get_footer();
-?>
+    <?php get_footer(); ?>
+</div>
+</body>
+</html>
