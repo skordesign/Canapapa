c5cb894d97e5dce97bbe4d220bd56b242d920d84
trunghieu <nguyenhieu.12a4@gmail.com>
2018-04-07 23:44:01 +0700

    setup themes canapapa

--- wp-content/themes/canapapa/404.php
+++
@@ -1,9 +0,0 @@
-
-<?php get_header('page'); ?>
-    <div class="container">
-      <div class="row">
-            <h2>404 NOT FOUND</h2>
-            <p>The article you were looking for was not found, but maybe try looking again!</p>
-      </div>
-    </div> <!-- /container -->
-<?php get_footer('page'); ?>
--- wp-content/themes/canapapa/content-none.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: HD088
- * Date: 3/10/2018
- * Time: 12:59 PM
- */
